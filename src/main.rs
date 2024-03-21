5ac777b072f3fa9d30661f4b8ed343e4ff0fb8ca
vsszhang <vsszhang@gmail.com>
2024-03-21 15:59:58 +0800

    feat: add game terminal  build logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        // clear window
+        ctx.cls();
+
+        // print something
+        ctx.print(1, 1, "Hello, flappy developer");
+    }
+}
+
+fn main() -> BError {
+    // build game terminal instance
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Bird")
+        .build()?;
+
+    // game main loop
+    main_loop(context, State {})
 }
