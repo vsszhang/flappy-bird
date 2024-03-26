6b9d2d11cc606ee4ec7af3064b6300f69854bd83
vsszhang <vsszhang@gmail.com>
2024-03-26 14:26:24 +0800

    feat: add game state control

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,73 @@
 use bracket_lib::prelude::*;
 
-struct State {}
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    // set game initial state
+    fn new() -> State {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Bird");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        // listen to keyboard input
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    // go to 'play' state
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+
+    fn play(&mut self) {
+        self.mode = GameMode::End;
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        // listen to keyboard input
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
 
 impl GameState for State {
+    // tick fn controls game state
     fn tick(&mut self, ctx: &mut BTerm) {
-        // clear window
-        ctx.cls();
-
-        // print something
-        ctx.print(1, 1, "Hello, flappy developer");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(),
+            GameMode::End => self.dead(ctx),
+        }
     }
 }
 
@@ -19,5 +78,5 @@ fn main() -> BError {
         .build()?;
 
     // game main loop
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
