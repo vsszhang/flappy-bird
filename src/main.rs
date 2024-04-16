e93f5394b1401efb2f2c457ab780b545bc651ab4
vsszhang <vsszhang@gmail.com>
2024-04-16 17:40:29 +0800

    feat: add Obstacle logic and update play logic

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ const SCREEN_HEIGHT: i32 = 50;
 const FRAME_DURATION: f32 = 75.0; // ms, as a unit time, do something every unit time
 
 struct Player {
-    x: i32,
+    x: i32, // horizontal coordinate of whole game world
     y: i32,
     velocity: f32,
 }
@@ -47,10 +47,53 @@ impl Player {
     }
 }
 
+struct Obstacle {
+    x: i32,     // horizontal coordinate of whole game world
+    gap_y: i32, // y: hole in the middle of the obstacle (obstacle hole, as following)
+    size: i32,  // obstacle hole size
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let obstacle_screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        // render first half of obstacle
+        for y in 0..self.gap_y - half_size {
+            ctx.set(obstacle_screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+
+        // render bottom half of obstacle
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(obstacle_screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+    }
+
+    fn hit_obstacle(&mut self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+
+        let x_overlap = self.x == player.x;
+        let y_overlap = player.y < self.gap_y - half_size || player.y > self.gap_y + half_size;
+
+        x_overlap && y_overlap
+    }
+}
+
 struct State {
     player: Player,
     frame_time: f32, // 游戏经过多少帧以后，累计了多少时间
     mode: GameMode,
+    obstacle: Obstacle,
+    score: i32,
 }
 
 impl State {
@@ -60,6 +103,8 @@ impl State {
             player: Player::new(5, 25),
             frame_time: 0.0,
             mode: GameMode::Menu,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            score: 0,
         }
     }
 
@@ -82,6 +127,10 @@ impl State {
     // go to 'play' state
     fn restart(&mut self) {
         self.mode = GameMode::Playing;
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.score = 0;
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
@@ -98,9 +147,17 @@ impl State {
         }
 
         self.player.render(ctx);
+        self.obstacle.render(ctx, self.player.x);
         ctx.print(0, 0, "Press Space to Flap");
+        ctx.print(0, 1, format!("Your score: {}", self.score));
 
-        if self.player.y > SCREEN_HEIGHT {
+        // add score and generate next obstacle
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
@@ -108,7 +165,8 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead");
-        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(6, format!("Your final score is: {} !!", self.score));
+        ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
 
         // listen to keyboard input
