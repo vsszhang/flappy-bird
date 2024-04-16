7cf62e9ccc53301b41418703c9f0c3000c7e51ff
vsszhang <vsszhang@gmail.com>
2024-04-16 14:59:00 +0800

    feat: add new player struct and update play logic

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,50 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0; // ms, as a unit time, do something every unit time
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+
+        self.y += self.velocity as i32;
+        self.x += 1;
+
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 struct State {
+    player: Player,
+    frame_time: f32, // 游戏经过多少帧以后，累计了多少时间
     mode: GameMode,
 }
 
@@ -14,6 +57,8 @@ impl State {
     // set game initial state
     fn new() -> State {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
@@ -39,8 +84,25 @@ impl State {
         self.mode = GameMode::Playing;
     }
 
-    fn play(&mut self) {
-        self.mode = GameMode::End;
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms; // 上次 tick fn 被调用的时候到现在时间的段时间
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press Space to Flap");
+
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn dead(&mut self, ctx: &mut BTerm) {
@@ -65,7 +127,7 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         match self.mode {
             GameMode::Menu => self.main_menu(ctx),
-            GameMode::Playing => self.play(),
+            GameMode::Playing => self.play(ctx),
             GameMode::End => self.dead(ctx),
         }
     }
